dfd6323ce0c9a74dd0c818d751ea8c47be6df5d7
chetanmadiwalar <chetanmadiwalar1947@gmail.com>
2025-05-24 17:10:05 +0530

    updated db.js

--- db/db.js
+++ db/db.js
@@ -7,6 +7,9 @@ const db = async () => {
     try {
         mongoose.set('strictQuery', false)
         await mongoose.connect(MONGO_URL)
+        .then(() => {
+            mongoose.connection.db.command({ ping: 1 });
+        })
         console.log('Db Connected')
     } catch (error) {
         console.log(`${error}`);
