7863fa6959836f21dd0b8db6c9503b2b257f7242
chetanmadiwalar <chetanmadiwalar1947@gmail.com>
2025-05-24 16:51:37 +0530

    updated db.js

--- db/db.js
+++ db/db.js
@@ -6,10 +6,7 @@ const MONGO_URL = process.env.MONGO_URL
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+        await mongoose.connect(MONGO_URL)
         console.log('Db Connected')
     } catch (error) {
         console.log(`${error}`);
